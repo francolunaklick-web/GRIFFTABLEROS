a8966239d234a1ea57f74cfada6f42d4b4b3732c
francolunaklick-web <franco.lunaklick@gmail.com>
2026-07-24 15:52:21 -0300

    actualizar

--- facturacion/datos.js
+++ facturacion/datos.js
@@ -1,6 +1,6 @@
 // Auto-generado por actualizar.py - No editar manualmente
 window.DATOS = {
-  "timestamp": "2026-07-21 09:13",
+  "timestamp": "2026-07-24 15:51",
   "facturas": [
     {
       "fecha": "2026-05-01 00:00:00",
