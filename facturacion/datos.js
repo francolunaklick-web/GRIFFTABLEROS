d3910dca974e3696f941ef779d8670b8f0c01c4c
francolunaklick-web <franco.lunaklick@gmail.com>
2026-09-24 10:59:56 -0300

    actualizar cheques

--- facturacion/datos.js
+++ facturacion/datos.js
@@ -1,6 +1,6 @@
 // Auto-generado por actualizar.py - No editar manualmente
 window.DATOS = {
-  "timestamp": "2026-09-17 15:59",
+  "timestamp": "2026-09-24 10:57",
   "facturas": [
     {
       "fecha": "2026-05-01 00:00:00",
